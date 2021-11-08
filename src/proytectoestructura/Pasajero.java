3fbe88bd1f9f75a741935079706c4e4a90c843f1
Gabrielmong <84334533+Gabrielmong@users.noreply.github.com>
2021-11-08 10:51:35 -0600

    Another lmao

--- src/proytectoestructura/Pasajero.java
+++ src/proytectoestructura/Pasajero.java
@@ -1,14 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proytectoestructura;
-
-/**
- *
- * @author gabri
- */
 public class Pasajero {
+    private String nombre;
+    private int edad;
+    private int cedula;
+    private Boolean vacunado;
+
+    public Pasajero() {
+        this.nombre = "";
+        this.edad = 0;
+        this.cedula = 0;
+        this.vacunado = false;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public Boolean getVacunado() {
+        return vacunado;
+    }
+
+    public void setVacunado(Boolean vacunado) {
+        this.vacunado = vacunado;
+    }
+    
     
 }
