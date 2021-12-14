51eacb7360bbcb27f6dc7348b03162025c34eef7
Gabrielmong <84334533+Gabrielmong@users.noreply.github.com>
2021-12-14 16:06:33 -0600

    lol

--- src/proytectoestructura/Estructuras.java
+++ src/proytectoestructura/Estructuras.java
@@ -4,25 +4,68 @@ import javax.swing.JOptionPane;
 
 public class Estructuras {
 
-    private NodoCola inicio;
-    private NodoCola fin;
+    private NodoCola inicioC;
+    private NodoCola finC;
+    private NodoLDC inicioLDC;
+    private NodoLDC finLDC;
+    private NodoLDS inicioLDS;
+    private NodoLDS finLDS;
+    private NodoLS inicioLS;
+    private NodoLS finLS;
+    private NodoLSC inicioLSC;
+    private NodoLSC finLSC;
+    private NodoPila Top;
 
     public Estructuras() {
-        this.inicio = null;
-        this.fin = null;
+        this.inicioC = null;
+        this.finC = null;
+        this.inicioLDC = null;
+        this.finLDC = null;
+        this.inicioLDS = null;
+        this.finLDS = null;
+        this.inicioLS = null;
+        this.finLS = null;
+        this.inicioLSC = null;
+        this.finLSC = null;
+        this.Top = null;
     }
 
-    public boolean esVacía() {
-        return inicio == null;
+    /*
+            Métodos booleanos
+     */
+    public boolean ColaEsVacía() {
+        return inicioC == null;
     }
 
-    public void agregar() {
+    public boolean LDCEsVacía() {
+        return inicioLDC == null;
+    }
+
+    public boolean LDSEsVacía() {
+        return inicioLDS == null;
+    }
+
+    public boolean LSEsVacía() {
+        return inicioLS == null;
+    }
+
+    public boolean LSCEsVacía() {
+        return inicioLSC == null;
+    }
+
+    public boolean PilaEsVacía() {
+        return Top == null;
+    }
+
+    /* 
+            Métodos para agregar a las estructuras
+     */
+    public void agregarCola() {   // Cola
         Pasajero p = new Pasajero();
         NodoCola nc = new NodoCola();
         p.setCedula(Integer.parseInt(JOptionPane.showInputDialog(null, "Número de cédula: ")));
         p.setEdad(Integer.parseInt(JOptionPane.showInputDialog(null, "Edad: ")));
         p.setNombre(JOptionPane.showInputDialog(null, "Nombre del pasajero: "));
-        
         int tempop = 1;
         while (tempop != 0) {
             int temp = Integer.parseInt(JOptionPane.showInputDialog(null, "Está Vacunado? \n1. Sí   2. No"));
@@ -40,31 +83,145 @@ public class Estructuras {
                     break;
             }
         }
-
         nc.setPasajero(p);
-        if (esVacía()) {
-            inicio = nc;
-            fin = nc;
+        if (ColaEsVacía()) {
+            inicioC = nc;
+            finC = nc;
         } else {
-            fin.setSiguiente(nc);
-            fin = nc;
+            finC.setSiguiente(nc);
+            finC = nc;
 
         }
     }
 
-    public void desencolar() {
-        if (!esVacía()) {
-            inicio = inicio.getSiguiente();
+    public void agregarLDC() {   //Lista Doble Circular
+        Dato d = new Dato();
+
+        NodoLDC nLDC = new NodoLDC();
+        nLDC.setElemento(d);
+        if (LDCEsVacía()) {
+            inicioLDC = nLDC;
+            finLDC = nLDC;
+            finLDC.setSiguiente(inicioLDC);
+            inicioLDC.setAnterior(finLDC);
+        } else {
+            NodoLDC aux = inicioLDC;
+            nLDC.setSiguiente(aux.getSiguiente());
+            nLDC.setAnterior(aux);
+            aux.setSiguiente(nLDC);
+            nLDC.getSiguiente().setAnterior(nLDC);
+        }
+    }
+
+    public void agregarLDS() {  // Lista Doble Simple   ***No estoy totalmente seguro me duele la cabeza xd
+        Dato d = new Dato();
+
+        NodoLDS nLDS = new NodoLDS();
+        nLDS.setElemento(d);
+        if (LDCEsVacía()) {
+            inicioLDS = nLDS;
+            finLDS = nLDS;
+        } else {
+            NodoLDS aux = inicioLDS;
+            nLDS.setSiguiente(aux.getSiguiente());
+            nLDS.setAnterior(aux);
+            aux.setSiguiente(nLDS);
+            nLDS.getSiguiente().setAnterior(nLDS);
+        }
+    }
+
+    public void agregarLS() {    //Lista Simple
+        Dato d = new Dato();
+
+        NodoLS nLS = new NodoLS();
+        if (LSEsVacía()) {
+            inicioLS = nLS;
+        } else {
+            NodoLS aux = inicioLS;
+            nLS.setSiguiente(aux.getSiguiente());
+            aux.setSiguiente(nLS);
+        }
+    }
+
+    public void agregarLSC() {   //Lista Simple Circular
+        Dato d = new Dato();
+
+        NodoLSC nLSC = new NodoLSC();
+        if (LSCEsVacía()) {
+            inicioLSC = nLSC;
+            finLSC = nLSC;
+            finLSC.setSiguiente(inicioLSC);
+        } else {
+            NodoLSC aux = inicioLSC;
+            nLSC.setSiguiente(aux.getSiguiente());
+            aux.setSiguiente(nLSC);
+        }
+    }
+
+    public void agregarPila() {  //Pila
+        Dato d = new Dato();
+
+        NodoPila nP = new NodoPila();
+        if (PilaEsVacía()) {
+            Top = nP;
+        } else {
+            nP.setNext(Top);
+            Top = nP;
+        }
+    }
+
+    /*
+            Métodos para eliminar un elemento de las estructuras
+     */
+    public void desencolar() {  //Cola
+        if (!ColaEsVacía()) {
+            inicioC = inicioC.getSiguiente();
             JOptionPane.showMessageDialog(null, "El elemento de extraído");
         } else {
             JOptionPane.showMessageDialog(null, "Cola vacía, no se puede extraer");
         }
     }
 
-    public void mostrarCola() {
-        if (!esVacía()) {
+    public void quitarLDC() {   //Lista Doble Circular
+        if (LDCEsVacía()) {
+            inicioLDC = inicioLDC.getSiguiente();
+            finLDC.setSiguiente(inicioLDC);
+        }
+    }
+
+    public void quitarLDS() {   //Lista Doble Simple
+        if (LSEsVacía()) {
+            inicioLS = inicioLS.getSiguiente();
+        }
+    }
+
+    public void quitarLS() {   //Lista Simple
+        if (LSCEsVacía()) {
+            inicioLS = inicioLS.getSiguiente();
+        }
+    }
+
+    public void quitarLSC() {   //Lista Simple Circular
+        if (LSCEsVacía()) {
+            inicioLSC = inicioLSC.getSiguiente();
+            finLSC.setSiguiente(inicioLSC);
+        }
+    }
+
+    public void quitarPila() {   //Pila
+        if (PilaEsVacía()) {
+            Top = Top.getNext();
+        }
+    }
+
+
+    /*
+            Métodos para mostrar las estructuras
+     */
+    public void mostrarCola() {    //Cola
+        if (!ColaEsVacía()) {
             String s = "";
-            NodoCola aux = inicio;
+            NodoCola aux = inicioC;
             while (aux != null) {
                 s = s + aux.getPasajero().getNombre() + aux.getPasajero().getEdad() + " " + "<--";
                 aux = aux.getSiguiente();
@@ -72,6 +229,61 @@ public class Estructuras {
             JOptionPane.showMessageDialog(null, "La cola contiene: \n" + s);
         } else {
             JOptionPane.showMessageDialog(null, "No existen elementos para mostrar.");
+        }
+    }
+
+    public void mostrarLDC() {   //Lista Doble Circular
+        if (!LDCEsVacía()) {
+            String s = "";
+            NodoLDC aux = inicioLDC;
+            while (aux != null) {
+                s = s + aux.getElemento();
+            }
+            JOptionPane.showMessageDialog(null, s);
+        }
+    }
+
+    public void mostrarLDS() {   //Lista Doble Simple
+        if (!LDSEsVacía()) {
+            String s = "";
+            NodoLDS aux = inicioLDS;
+            while (aux != null) {
+                s = s + aux.getElemento();
+            }
+            JOptionPane.showMessageDialog(null, s);
+        }
+    }
+
+    public void mostrarLS() {   //Lista Simple
+        if (!LSEsVacía()) {
+            String s = "";
+            NodoLS aux = inicioLS;
+            while (aux != null) {
+                s = s + aux.getDatos();
+            }
+            JOptionPane.showMessageDialog(null, s);
+        }
+    }
+
+    public void mostrarLSC() {   //Lista Simple Circular
+        if (!LSCEsVacía()) {
+            String s = "";
+            NodoLSC aux = inicioLSC;
+            while (aux != null) {
+                s = s + aux.getDatos();
+            }
+            JOptionPane.showMessageDialog(null, s);
+        }
+    }
+
+    public void mostrarPila() {   //Pilä
+        if (!PilaEsVacía()) {
+            String s = "";
+            NodoPila aux = Top;
+            while (aux != null) {
+                s = s + aux.getDato();
+            }
+            JOptionPane.showMessageDialog(null, s);
         }
     }
 }
